37628908895b7e4d1db9a42a140eeab86b5bc46b
McMurrays008 <sam@mcmurrayshaulage.com>
2026-09-11 13:19:55 +0100

    Delete install_automation.bat

--- install_automation.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Install TPN Dedicated Day Automation
-cd /d "%~dp0"
-python -m pip install --upgrade pip
-python -m pip install playwright openpyxl
-python -m playwright install chromium
-echo.
-echo Installation complete.
-pause
